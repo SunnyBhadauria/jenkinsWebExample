002d6c70a10400bfe12251c49890321d4d32ae18
sunnyBhadauria <sunnybhadauria29@gmail.com>
2021-04-22 12:08:25 +0530

    my web commit

---
+++ src/main/java/com/coforge/webExampleJenkins/HelloWorldExample.java
@@ -0,0 +1,47 @@
+package com.coforge.webExampleJenkins;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldExample
+ */
+public class HelloWorldExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+//    public HelloWorldExample() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		pw.println("<html>");
+		pw.println("<body>");
+		pw.println("<h2>JAI GANESH TO SERVLET<h2>");
+		pw.println("</body>");
+		pw.println("</html>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
